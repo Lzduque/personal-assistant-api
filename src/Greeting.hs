ad2e6137062b2a36592316f62d44f5e14e89c266
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2020-06-03 13:58:38 -0400

    refactor greeting

--- src/Greeting.hs
+++ src/Greeting.hs
@@ -2,11 +2,16 @@ module Greeting (greeting) where
 
 import Data.Time (ZonedTime, defaultTimeLocale, formatTime)
 
+zonedHour :: ZonedTime -> Int
+zonedHour timeNow = read (formatTime defaultTimeLocale "%H" timeNow) :: Int
+
+userName :: String
+userName = "Leticia"
+
 greeting :: ZonedTime -> String
 greeting timeNow 
-    | hour >= 18 = "Good night, Leticia!"
-    | hour >= 12 = "Good afternoon, Leticia!"
-    | otherwise = "Good morning, Leticia!"
+    | hour >= 18 = "Good night, " ++ userName ++ "!"
+    | hour >= 12 = "Good afternoon, " ++ userName ++ "!"
+    | otherwise = "Good morning, " ++ userName ++ "!"
     where
-        time = formatTime defaultTimeLocale "%H" timeNow
-        hour = read time :: Int
+        hour = zonedHour timeNow
